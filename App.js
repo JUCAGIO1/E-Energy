52d5b9ca63afe080247126898bd6dd8c57f6d4c5
Giovanni <ushaolinmatadordeporco@gmail.com>
2025-08-27 14:45:59 -0300

    Revert "terminar depois essas telas"
    This reverts commit 51ae7a7ed8f604cd27067d0d35ba2d2c90900b8d.

--- App.js
+++ App.js
@@ -2,11 +2,10 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import TelaDeLogin from "./src/screens/TelaDeLogin";
-import TelaDeAcesso from "./src/screens/TelaDeAcesso";
-import TelaDeCadastro from "./src/screens/TelaDeCadastro";
-import TelaCasas from "./src/screens/TelaCasas";       // ✅
-import TelaComodos from "./src/screens/TelaComodos";   // ✅
+import TelaDeAcesso from "./src/screens/TelaDeAcesso";  
+import TelaDeCadastro from "./src/screens/TelaDeCadastro";  // idem
+import TelaDeLogin from "./src/screens/TelaDeLogin"; // nome sem espaço! 
+
 
 const Stack = createNativeStackNavigator();
 
@@ -17,8 +16,6 @@ export default function App() {
         <Stack.Screen name="Login" component={TelaDeLogin} />
         <Stack.Screen name="TelaDeAcesso" component={TelaDeAcesso} />
         <Stack.Screen name="TelaDeCadastro" component={TelaDeCadastro} />
-        <Stack.Screen name="TelaCasas" component={TelaCasas} />
-        <Stack.Screen name="TelaComodos" component={TelaComodos} />
       </Stack.Navigator>
     </NavigationContainer>
   );
